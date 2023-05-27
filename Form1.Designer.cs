a09f026c6553b1807a716b3601ccb18db3a37840
Enes Birdal ÇAVUŞ <Enes Birdal AVU@BUKET>
2023-05-27 22:24:12 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Dinamik_Sinema_Salonu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 236);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "***********\r\n** ** ** **\r\n** ** ** **\r\n\r\n***********\r\n*** *** ***\r\n*** *** ***\r\n\r" +
+    "\n** ** ** **\r\n***********";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 267);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 149);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OLUŞTUR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.panel1.Location = new System.Drawing.Point(244, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(404, 404);
+            this.panel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 443);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "DİNAMİK SİNEMA TİYATRO SALONU";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dinamik_Sinema_Salonu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int say = 0;
+
+            
+            panel1.Controls.Clear();
+
+            
+            for (int i = 0; i < textBox1.Lines.Count(); i++)
+            {
+                for (int j = 0; j < textBox1.Lines[i].Count(); j++)
+                {
+                    string satir = textBox1.Lines[i];
+
+                    
+                    if (satir[j] == '*')
+                    {
+                        
+                        Button nesne = new Button();
+                        nesne.Name = "buton" + i;
+                        nesne.Text = (++say).ToString();
+                        nesne.BackColor = Color.Red;
+                        nesne.Width = nesne.Height = 30;
+                        nesne.Left = 35 * j;
+                        nesne.Top = 35 * i;
+
+                       
+                        nesne.Click += koltukSecildi;
+
+                        
+                        panel1.Controls.Add(nesne);
+                    }
+                }
+            }
+        }
+        void koltukSecildi(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (btn.BackColor == Color.Red)
+                btn.BackColor = Color.Aqua;
+            else
+                btn.BackColor = Color.Red;
+
+        }
+    }
+}
